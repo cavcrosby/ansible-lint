3477eba34f2f28f759883659e4318770635986a8
Conner Crosby <cavcrosby@gmail.com>
2024-12-12 11:30:16 -0500

    Treat the default inventory_files as host list (#4448)
    The --inventory option to the ansible-playbook executable can accept a
    value that is comma-separated. This comma-separated value is presumed to
    be a host list and not a list of inventory files. This was the behavior
    for the inventory_files variable before 6fbb12e9. There wasn't a reason
    for its removal and it was an oversight made.

--- src/ansiblelint/runner.py
+++ src/ansiblelint/runner.py
@@ -514,7 +514,7 @@ class Runner:
             # To avoid noisy warnings we pass localhost as current inventory:
             # [WARNING]: No inventory was parsed, only implicit localhost is available
             # [WARNING]: provided hosts list is empty, only localhost is available. Note that the implicit localhost does not match 'all'
-            inventory_files = ["localhost"]
+            inventory_files = ["localhost,"]
 
         return inventory_files
 
